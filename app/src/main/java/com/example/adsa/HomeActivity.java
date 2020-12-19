b256d4e738a7bb11b8485913cf811ac2dbfa7a52
jai-kherajani <jai.kherajani@appdirect.com>
2020-12-19 13:40:43 +0530

    add recycler view and icons for home page
    Signed-off-by: jai-kherajani <jai.kherajani@appdirect.com>

--- app/src/main/java/com/example/adsa/HomeActivity.java
+++ app/src/main/java/com/example/adsa/HomeActivity.java
@@ -1,14 +1,42 @@
 package com.example.adsa;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private final String TAG = "HOME ACTIVITY";
+    private final ArrayList<String> itemNames = new ArrayList<>();
+    private final ArrayList<Integer> itemImages = new ArrayList<>();
+    private final ArrayList<Activity> itemActivities = new ArrayList<>();
+
+    private RecyclerView recyclerView;
+    private HomeAdapter homeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar myToolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Collections.addAll(itemNames, "ID Card", "Directory", "Calendar", "Time Sheets", "Wellbeing", "Others");
+        Collections.addAll(itemImages, R.drawable.id_card, R.drawable.team, R.drawable.calendar, R.drawable.timeline, R.drawable.health, R.drawable.options);
+        homeAdapter = new HomeAdapter(HomeActivity.this, itemNames, itemImages, itemActivities);
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL));
+        Log.d(TAG, "HomeAdapter Size : " + homeAdapter.getItemCount());
+        recyclerView.setAdapter(homeAdapter);
     }
 }
--- app/src/main/java/com/example/adsa/HomeAdapter.java
+++ app/src/main/java/com/example/adsa/HomeAdapter.java
@@ -1,7 +1,13 @@
 package com.example.adsa;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,27 +18,50 @@ public class HomeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private final ArrayList<String> itemNames;
     private final ArrayList<Integer> itemImages;
+    private final ArrayList<Activity> itemActivities;
     private final Context context;
 
-    public HomeAdapter(Context context, ArrayList<String> itemNames, ArrayList<Integer> itemImages) {
+    public HomeAdapter(Context context, ArrayList<String> itemNames, ArrayList<Integer> itemImages, ArrayList<Activity> itemActivities) {
         this.itemImages = itemImages;
         this.itemNames = itemNames;
         this.context = context;
+        this.itemActivities = itemActivities;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item, parent, false);
+        return new RecyclerHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        RecyclerHolder recyclerHolder = (RecyclerHolder) holder;
+        recyclerHolder.label.setText(itemNames.get(position));
+        recyclerHolder.image.setImageResource(itemImages.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itemNames.size();
+    }
+
+    public class RecyclerHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private final TextView label;
+        private final ImageView image;
+
+        public RecyclerHolder(View itemView) {
+            super(itemView);
+            label = itemView.findViewById(R.id.item_name);
+            image = itemView.findViewById(R.id.item_image);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(view.getContext(), itemNames.get(getAdapterPosition()) + " Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
